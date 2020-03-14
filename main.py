b6c3551c5f403eae723c441876beaaf1f6c4652e
Dmitry <dimk00z@gmail.com>
2020-03-14 12:07:46 +0300

    Исправление ошибок
    Удалены лишние файлы из репозитория
    Зафиксированы версии пакетов
    Вынесен конфиг из кода
    Высушены названия
    Выделен содержимое блочных тегов
    Использан dict.item()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import argparse
 from datetime import datetime
 from pandas import read_excel
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -8,22 +9,22 @@ YEAR_OF_CREATION_OF_THE_WINERY = 1920
 
 
 def load_drinks_from_excel_file(file_name):
-    drinks_data = read_excel(
+    drinks_excel_table = read_excel(
         file_name,
         sheet_name='Лист1',
         na_values=['N/A', 'NA'],
         keep_default_na=False
     )
     drink_types = defaultdict(list)
-    for drink_data in drinks_data.to_dict(orient='record'):
-        drink_types[drink_data['Категория']].append({
-            'name': drink_data['Название'],
-            'price': drink_data['Цена'],
-            'grape_variety': drink_data['Сорт'],
-            'image': drink_data['Картинка'],
-            'sales': drink_data['Акция']
+    for drink in drinks_excel_table.to_dict(orient='record'):
+        drink_types[drink['Категория']].append({
+            'name': drink['Название'],
+            'price': drink['Цена'],
+            'grape_variety': drink['Сорт'],
+            'image': drink['Картинка'],
+            'sales': drink['Акция']
         })
-    return OrderedDict(sorted(drink_types.items()))
+    return dict(sorted(drink_types.items()))
 
 
 def generate_html_template(template_file_name, drinks_file_name):
@@ -41,12 +42,21 @@ def generate_html_template(template_file_name, drinks_file_name):
     return rendered_page
 
 
+def read_links_from_args():
+    args_parser = argparse.ArgumentParser(description='''This script can
+    load wine price list from file,
+    generate html page and start simple web server''')
+    args_parser.add_argument("wine_price_file", type=str,
+                             help="Enter file name")
+    return args_parser.parse_args().wine_price_file
+
+
 def main():
     template_file_name = 'template.html'
-    drinks_file_name = 'wine3.xlsx'
+    drinks_file_name = read_links_from_args()
     rendered_page = generate_html_template(
         template_file_name, drinks_file_name)
-    drinks = load_drinks_from_excel_file('wine3.xlsx')
+    drinks = load_drinks_from_excel_file(drinks_file_name)
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
